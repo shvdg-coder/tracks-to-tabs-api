716a6c8c4734c6ce9329aeede695edb078e9edde
StanHvdG <stanhvdg@gmail.com>
2024-08-03 19:36:29 +0200

    remove database testing

--- internal/integration_tests/create_test.go
+++ internal/integration_tests/create_test.go
@@ -1,18 +1,9 @@
 package integration_tests
 
 import (
-	"github.com/shvdg-dev/tunes-to-tabs-api/internal/integration_tests/testable"
 	"testing"
 )
 
-func TestTableCreation(t *testing.T) {
-	db, err := testable.NewTestDatabase()
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestDatabaseSetup(t *testing.T) {
 
-	defer db.Teardown()
-
-	db.CreateAll()
-	db.DropAll()
 }
