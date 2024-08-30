c34df39e4dcf42aad9f40f66c424ad3297a32356
StanHvdG <stanhvdg@gmail.com>
2024-08-30 12:51:23 +0200

    Refactor createExpectedArtists to dynamically load from CSV
    Revised `createExpectedArtists` to dynamically generate expected artists from a CSV file. This removes hardcoded artist data, enhancing maintainability and flexibility for integration tests.

--- internal/integration_tests/artists_test.go
+++ internal/integration_tests/artists_test.go
@@ -9,8 +9,8 @@ import (
 
 // ExpectedArtist contains the data of what a models.Artist is expected to have.
 type ExpectedArtist struct {
-	ID             string
-	Name           string
+	ID string
+	*models.ArtistEntry
 	TrackCount     int
 	ReferenceCount int
 	ResourceCount  int
@@ -48,15 +48,7 @@ func TestGetArtists(t *testing.T) {
 		t.Errorf("expected %d artists, got %d", len(artistIDs), len(actualArtists))
 	}
 
-	testFieldsOfArtists(t, actualArtists, createExpectedArtists())
-}
-
-// createExpectedArtists constructs and returns a slice of ExpectedArtist objects for use in test cases.
-func createExpectedArtists() []*ExpectedArtist {
-	return []*ExpectedArtist{
-		{ID: "9b5ed980-340a-4b69-8d47-92e358992657", Name: "Knocked Loose", TrackCount: 2, ReferenceCount: 3, ResourceCount: 2},
-		{ID: "1d7edc80-526b-4f74-a5de-657a7c9e0521", Name: "Ice Nine Kills", TrackCount: 2, ReferenceCount: 3, ResourceCount: 2},
-	}
+	testFieldsOfArtists(t, actualArtists, createExpectedArtists(t))
 }
 
 // testFieldsOfArtists tests the fields of multiple artist objects by comparing the actual artists to the expected ones.
@@ -74,9 +66,9 @@ func testFieldsOfArtist(t *testing.T, actualArtist *models.Artist, expectedArtis
 		t.Errorf("expected ID to be %s, got %s", expectedArtist.ID, actualArtist.ID)
 	}
 
-	// Check Artist Title
+	// Check Artist Name
 	if actualArtist.Name != expectedArtist.Name {
-		t.Errorf("expected Title to be %s, got %s", expectedArtist.Name, actualArtist.Name)
+		t.Errorf("expected Name to be %s, got %s", expectedArtist.Name, actualArtist.Name)
 	}
 
 	// Check Tracks
@@ -93,4 +85,46 @@ func testFieldsOfArtist(t *testing.T, actualArtist *models.Artist, expectedArtis
 	if len(actualArtist.Resources) != expectedArtist.ResourceCount {
 		t.Errorf("expected %d Resources, got %d", expectedArtist.ResourceCount, len(actualArtist.Resources))
 	}
+}
+
+// createExpectedArtists constructs and returns a slice of ExpectedArtist objects for use in test cases.
+func createExpectedArtists(t *testing.T) []*ExpectedArtist {
+	expectedArtists := make([]*ExpectedArtist, 0)
+
+	artistsMap := createArtistsFromCSV(t, artistsCSV)
+	for id, artist := range artistsMap {
+		expectedArtist := &ExpectedArtist{
+			ID:             id,
+			ArtistEntry:    artist,
+			TrackCount:     2,
+			ReferenceCount: 3,
+			ResourceCount:  2,
+		}
+		expectedArtists = append(expectedArtists, expectedArtist)
+	}
+
+	return expectedArtists
+}
+
+// createArtistsFromCSV creates a map of artists where the key is the ID and the value a models.ArtistEntry.
+func createArtistsFromCSV(t *testing.T, filePath string) map[string]*models.ArtistEntry {
+	artistsMap := make(map[string]*models.ArtistEntry)
+
+	records, err := logic.GetCSVRecords(filePath, false)
+	if err != nil {
+		t.Fatalf("error occurred during the creation of artists from a CSV: %s", err.Error())
+	}
+
+	for _, record := range records {
+		artistID, _ := logic.StringToUUID(record[0])
+
+		artist := &models.ArtistEntry{
+			ID:   artistID,
+			Name: record[1],
+		}
+
+		artistsMap[artistID.String()] = artist
+	}
+
+	return artistsMap
 }
