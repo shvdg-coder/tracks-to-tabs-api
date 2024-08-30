5eb6e4d2ccd20c78ff742b332627807d42837ba4
StanHvdG <stanhvdg@gmail.com>
2024-08-30 09:21:19 +0200

    Add detailed validation for artist fields in tests
    Refactored integration tests to include checks for individual artist fields such as ID, Name, Tracks, References, and Resources. Added helper functions to construct expected artist data and to compare actual artist objects against this data.

--- internal/integration_tests/artists_test.go
+++ internal/integration_tests/artists_test.go
@@ -3,8 +3,18 @@ package integration_tests
 import (
 	logic "github.com/shvdg-dev/base-logic/pkg"
 	"github.com/shvdg-dev/tracks-to-tabs-api/pkg"
+	"github.com/shvdg-dev/tracks-to-tabs-api/pkg/models"
 	"testing"
 )
+
+// ExpectedArtist contains the data of what a models.Artist is expected to have.
+type ExpectedArtist struct {
+	ID             string
+	Name           string
+	TrackCount     int
+	ReferenceCount int
+	ResourceCount  int
+}
 
 // TestGetArtists tests whether artists can be inserted and retrieved cascading.
 func TestGetArtists(t *testing.T) {
@@ -26,13 +36,61 @@ func TestGetArtists(t *testing.T) {
 	api := pkg.NewDataAPI(dbEnv)
 
 	// Execute
-	artists, err := api.GetArtists(artistIDs...)
+	actualArtists, err := api.GetArtists(artistIDs...)
 	if err != nil {
 		t.Fatalf("error occurred during retrieval of artist: %s", err.Error())
 	}
 
 	// Test
-	if len(artists) != len(artistIDs) {
-		t.Errorf("expected %d number of artists, got %d", len(artistIDs), len(artists))
+	if len(actualArtists) != len(artistIDs) {
+		t.Errorf("expected %d number of actualArtists, got %d", len(artistIDs), len(actualArtists))
+	}
+
+	testFieldsOfArtists(t, actualArtists, createExpectedArtists())
+}
+
+// createExpectedArtists constructs and returns a slice of ExpectedArtist objects for use in test cases.
+func createExpectedArtists() []*ExpectedArtist {
+	return []*ExpectedArtist{
+		{ID: "9b5ed980-340a-4b69-8d47-92e358992657", Name: "Knocked Loose",
+			TrackCount: 2, ReferenceCount: 3, ResourceCount: 2},
+		{ID: "1d7edc80-526b-4f74-a5de-657a7c9e0521", Name: "Ice Nine Kills",
+			TrackCount: 2, ReferenceCount: 3, ResourceCount: 2},
+	}
+}
+
+// testFieldsOfArtists tests the fields of multiple artist objects by comparing the actual artists to the expected ones.
+func testFieldsOfArtists(t *testing.T, actualArtists []*models.Artist, expectedArtist []*ExpectedArtist) {
+	for i := 0; i < len(actualArtists); i++ {
+		testFieldsOfArtist(t, actualArtists[i], expectedArtist[i])
+	}
+}
+
+// testFieldsOfArtist tests the fields of a single artist object by comparing the actual artist to the expected one.
+func testFieldsOfArtist(t *testing.T, actualArtist *models.Artist, expectedArtist *ExpectedArtist) {
+	// Check ID
+	artistID, _ := logic.UUIDToString(actualArtist.ID)
+	if artistID != expectedArtist.ID {
+		t.Errorf("expected ID to be %s, got %s", expectedArtist.ID, actualArtist.ID)
+	}
+
+	// Check Artist Name
+	if actualArtist.Name != expectedArtist.Name {
+		t.Errorf("expected Name to be %s, got %s", expectedArtist.Name, actualArtist.Name)
+	}
+
+	// Check Tracks
+	if len(actualArtist.Tracks) != expectedArtist.TrackCount {
+		t.Errorf("expected %d Tracks, got %d", expectedArtist.TrackCount, len(actualArtist.Tracks))
+	}
+
+	// Check References
+	if len(actualArtist.References) != expectedArtist.ReferenceCount {
+		t.Errorf("expected %d References, got %d", expectedArtist.ReferenceCount, len(actualArtist.References))
+	}
+
+	// Check Resources
+	if len(actualArtist.Resources) != expectedArtist.ResourceCount {
+		t.Errorf("expected %d Resources, got %d", expectedArtist.ResourceCount, len(actualArtist.Resources))
 	}
 }
